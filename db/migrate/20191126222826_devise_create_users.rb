715962f9ee0a06e7e86fdae964710a4399ed8853
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-26 19:35:23 -0300

    User migration

--- db/migrate/20191126222826_devise_create_users.rb
+++ db/migrate/20191126222826_devise_create_users.rb
@@ -3,6 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :name,               null: false, default: ""
+      t.string :image,              null: true
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
