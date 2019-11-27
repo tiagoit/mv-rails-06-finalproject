1de5cd38eac81587f37bf748d24848907210b459
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-26 23:27:54 -0300

    adjust layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   resource :post
   devise_for :users, path: '', path_names: { sign_in: 'login', 
                                              sign_out: 'logout', 
