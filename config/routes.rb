d0a26a38b1593d03339e097893e69947f0f73f27
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-26 22:28:00 -0300

    configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: { sign_in: 'login', 
+                                             sign_out: 'logout', 
+                                             password: 'secret', 
+                                             confirmation: 'verification', 
+                                             unlock: 'unblock', 
+                                             registration: '', 
+                                             sign_up: 'sign-up' }
 end
