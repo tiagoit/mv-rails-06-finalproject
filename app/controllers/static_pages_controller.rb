5ad2659ce73d684254fcccb74a9c1a7e2158cd08
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-29 11:57:08 -0800

    static pages controller test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @users = User.all
     @post = current_user.posts.build
     @posts = Post.order(created_at: :desc)
   end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    sign_in users(:one)
+  end
+
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "div.card"
   end
 end
