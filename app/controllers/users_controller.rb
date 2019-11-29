6d58b00fa65490d3b6ac01a6127c55be2d60fb59
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-29 09:28:08 -0800

    user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @posts = Post.where(user_id: params[:id])
+  end
 end
