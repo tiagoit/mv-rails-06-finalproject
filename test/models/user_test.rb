cdc3508a6e25a5cd882efeb7dbac11126f9ea3b3
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-29 13:39:36 -0800

    user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user" do
+    user = User.new(name: "User test", email: "email@example.org", password: "password", password_confirmation: "password")
+    assert user.valid?
+  end
+
+  test "invalid user without name" do
+    user = User.new(email: "email@example.org", password: "password", password_confirmation: "password")
+    assert_not user.valid?
+  end
+
+  test "invalid user without email" do
+    user = User.new(name: "User test", password: "password", password_confirmation: "password")
+    assert_not user.valid?
+  end
+
+  test "invalid user bad password" do
+    user = User.new(name: "User test", email: "email@example.org", password_confirmation: "password")
+    assert_not user.valid?
+    user = User.new(name: "User test", email: "email@example.org", password: "password2", password_confirmation: "password")
+    assert_not user.valid?
+    user = User.new(name: "User test", email: "email@example.org", password: "password", password_confirmation: "password2")
+    assert_not user.valid?
+    user = User.new(name: "User test", email: "email@example.org")
+    assert_not user.valid?
+  end
+
+  test "has_many posts relationship" do
+    user = users(:one)
+    post = user.posts.build(content: "my post")
+    assert post.valid?
+    assert_not_empty user.posts
+    assert_not_nil user.posts
+  end
 end
