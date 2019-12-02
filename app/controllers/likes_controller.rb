35258943e5f760bf1f0b85932045e5fb1d9f9c7f
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-02 09:08:51 -0800

    create method finished

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    current_user.likes.build(post_id: params[:post])
 
+    if current_user.save
+      redirect_back fallback_location: root_path
+    else
+      flash[:danger] = "Something went wrong :("
+      redirect_to root_path
+    end
+  end
 end
