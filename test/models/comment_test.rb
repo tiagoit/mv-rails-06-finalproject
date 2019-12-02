ee94c97d180d443fc005140e677181f24ae54c3c
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-02 14:39:23 -0800

    comment model tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @post = posts(:one)
+    @content = "my text"
+  end
+  
+  test "valid comment" do
+    comment = Comment.new(user_id: @user.id, post_id: @post.id, content: @content)
+    assert comment.valid?
+  end
+
+  test "invalid comment without user" do
+    comment = Comment.new(user_id: nil, post_id: @post.id, content: @content)
+    assert_not comment.valid?
+  end
+
+  test "invalid comment without post" do
+    comment = Comment.new(user_id: @user.id, post_id: nil, content: @content)
+    assert_not comment.valid?
+  end
+
+  test "invalid comment without content" do
+    comment = Comment.new(user_id: @user.id, post_id: @post.id, content: "")
+    assert_not comment.valid?
+  end
 end
