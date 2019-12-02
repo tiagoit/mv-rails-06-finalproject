e24624475e4c01736252b3f084dffdeaee7e3998
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-02 12:55:45 -0800

    test likes model

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @post = posts(:one)
+  end
+  
+  test "valid like" do
+    like = Like.new(user_id: @user.id, post_id: @post.id)
+    assert like.valid?
+  end
+
+  test "invalid like without user" do
+    like = Like.new(post_id: @post.id)
+    assert_not like.valid?
+  end
+
+  test "invalid like without post" do
+    like = Like.new(user_id: @user.id)
+    assert_not like.valid?
+  end
 end
