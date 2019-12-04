1e2deec214bae6ae9971f2dccda21db049d3323c
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-04 10:09:35 -0800

    working on friendshop controller test

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -7,12 +7,22 @@ class FriendshipsControllerTest < ActionDispatch::IntegrationTest
     login_as @user = users(:one)
     @friend = users(:four)
     @three = users(:three)
-    @request = friendships(:three)
   end
   
   test 'should create a new friend request' do
     assert_difference 'Friendship.count', 1 do
       post friendship_request_path(@friend.id)
     end
+  end
+  
+  test 'should accept a friend request' do
+    assert_difference 'Friendship.count', 1 do
+      post friendship_request_path(@three.id)
+    end
+    logout
+    login_as @three
+    assert_not @three.friend?(@user.id)
+    post friendship_accept_path(@user.id)
+    assert @three.friend?(@user.id)
   end
 end
