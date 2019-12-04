e54c8d242c4ff5333ace84e933cf9ca160abd893
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-04 08:50:47 -0800

    friendship model tests

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @friend = users(:two)
+  end
+
+  test 'validad friendship request' do
+    request = Friendship.new(user_id: @user.id, friend_id: @friend.id)
+    assert request.valid?
+  end
+  
+  test 'invalid friendship request' do
+    request = Friendship.new(friend_id: @friend.id)
+    assert_not request.valid?
+    request = Friendship.new(user_id: @user.id)
+    assert_not request.valid?
+  end
 end
