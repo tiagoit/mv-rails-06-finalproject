a39a369bf6caddcb53cda4e3e1db79f13da2034b
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-04 09:41:59 -0800

    test friend request

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class FriendshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  
+  def setup
+    login_as @user = users(:one)
+    @friend = users(:four)
+    @three = users(:three)
+    @request = friendships(:three)
+  end
+  
+  test 'should create a new friend request' do
+    assert_difference 'Friendship.count', 1 do
+      post friendship_request_path(@friend.id)
+    end
+  end
 end
