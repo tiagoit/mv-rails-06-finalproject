2fb6cd15db89763c0fcbe73e8de19d381745e390
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-05 12:02:45 -0800

    new_session method

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,4 +56,12 @@ class User < ApplicationRecord
       user.image = auth.info.image
     end
   end
+
+  def new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
