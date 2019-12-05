6e8abdfa5d7aa96ed305dc2fe7a6160ece8bd702
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-05 08:52:49 -0800

    user model omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   ##############################################################################################################
   # RELATIONS
