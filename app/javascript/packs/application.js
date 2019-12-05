09a4c4467f913cf034f9c488dbe2f6bc919ee65f
Tiago Ferreira <tiferreira12@gmail.com>
2019-12-05 19:36:42 -0300

    import for font-awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
 require("jquery")
-require("@fortawesome/fontawesome-free/js/all")
+
+import "../../../node_modules/@fortawesome/fontawesome-free/js/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
