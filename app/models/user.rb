2b359297b5e975cdf77066c352c0a6d2f9bb1362
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-05 09:26:46 -0800

    from_omniauth method added to user model to get info from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,4 +47,13 @@ class User < ApplicationRecord
   def friend?(user_id)
     friends.include?(user_id)
   end
+  
+  def from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.image = auth.info.image
+    end
+  end
 end
