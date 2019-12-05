4cf95bb569a2457391c1e37c841cc18bb21ddb8e
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-05 08:47:48 -0800

    omniauth migration and configurations on devise initializaer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,7 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
