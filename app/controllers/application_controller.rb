7d61168678448bd15eed262de690d7686c171991
Tiago Ferreira <tiferreira12@gmail.com>
2019-12-05 17:43:48 -0300

    add forgery exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!, :notifications
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :notifications, unless: :devise_controller?
