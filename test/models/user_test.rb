b86810704b3acf0f0804cfd7b7c757167e7b9834
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-12-06 09:21:58 -0800

    user model methods tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user_one = users(:one)
+    @user_two = users(:two)
+    @user_three = users(:three)
+    @user_four = users(:four)
+    @user_five = users(:five)
+    @user_six = users(:six)
+    @user_seven = users(:seven)
+  end
+  
+  # validations  tests
   test "valid user" do
     user = User.new(name: "User test",
                     email: "email@example.org",
@@ -42,5 +53,56 @@ class UserTest < ActiveSupport::TestCase
     assert post.valid?
     assert_not_empty user.posts
     assert_not_nil user.posts
+  end
+
+  # methods tests
+  test '#friends' do
+    friends = @user_one.friends
+    friends.each do |friend|
+      assert [@user_two.id, @user_three.id].include?(friend)
+    end
+
+    friends.each do |friend|
+      assert_not [@user_four.id, @user_five.id].include?(friend)
+    end
+  end
+
+  test '#pending_acceptance' do
+    not_pending = [@user_two.id, @user_three.id, @user_four.id]
+    assert @user_one.pending_acceptance.include?(@user_five.id)
+    not_pending.each do |false_req|
+      assert_not @user_one.pending_acceptance.include?(false_req)
+    end
+  end
+  
+  test '#pending_friend_acceptance' do
+    not_pending = [@user_two.id, @user_three.id, @user_four.id, @user_five.id]
+    assert @user_one.pending_friend_acceptance.include?(@user_six.id)
+    
+    not_pending.each do |false_pending|
+      assert_not @user_one.pending_friend_acceptance.include?(false_pending)
+    end
+  end
+  
+  test '#request_friendship #accept_friendship' do
+    assert_not Friendship.find_by(user_id: @user_one.id, friend_id: @user_seven.id)
+    @user_one.request_friendship(@user_seven.id)
+    assert Friendship.find_by(user_id: @user_one.id, friend_id: @user_seven.id)
+    assert_not Friendship.find_by(user_id: @user_seven.id, friend_id: @user_one.id)
+    @user_seven.accept_friendship(@user_one.id)
+    assert Friendship.find_by(user_id: @user_seven.id, friend_id: @user_one.id)
+  end
+  
+  test '#friend?' do
+    friends = [@user_two.id, @user_three.id]
+    not_friends = [@user_four.id, @user_five.id, @user_six.id]
+    
+    friends.each do |friend|
+      assert @user_one.friend?(friend)
+    end
+    
+    not_friends.each do |not_friend|
+      assert_not @user_one.friend?(not_friend)
+    end
   end
 end
